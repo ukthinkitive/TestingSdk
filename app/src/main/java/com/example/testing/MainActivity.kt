b40beffcad16e5a757d69d37947c420b3dbc8583
lnv-254 <uttam.kumar@thinkitive.com>
2023-06-02 17:05:55 +0530

    Testing Completed

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.thinkitive.toasty.Toaster
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val toaster = Toaster()
+            toaster.simpleToast(this,"Hii this is Custom Message")
+        }
     }
 }
